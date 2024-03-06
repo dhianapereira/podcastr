7c4ce9b72fa735d46a55621d45beee3173120a5f
dhianapereira <dhianaslv@outlook.com>
2024-03-05 21:11:43 -0300

    chore: Update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch("https://localhost:3333/episodes");
+  const response = await fetch("http://localhost:3333/episodes");
   const data = await response.json();
 
   return {
